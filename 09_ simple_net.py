49ca33d864a6f12d653fae99808c46aac4e4f4e6
DaoChaShao <27882272@qq.com>
2025-10-09 01:04:24 +0800

    feat: add 09_simple_net.py as a template for a simple neural network implementation

---
+++ 09_ simple_net.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/8 22:23
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   09_ simple_net.py
+# @Desc     :   
+
+def main() -> None:
+    """ Main Function """
+    pass
+
+
+if __name__ == "__main__":
+    main()
