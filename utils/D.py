b9201fc93af97a5b25deee727a89f11476266cac
DaoChaShao <27882272@qq.com>
2025-10-09 20:47:53 +0800

    feat: add gradient descent optimization function and refactor gradient calculation methods

--- utils/D.py
+++ utils/D.py
@@ -6,7 +6,7 @@
 # @File     :   D.py
 # @Desc     :   
 
-from numpy import ndarray, zeros_like
+from numpy import ndarray, zeros_like, array
 
 
 def central_diff(f, x, h: float = 1e-5) -> float:
@@ -31,21 +31,21 @@ def exact_gradient(x):
     return 0.02 * x + 0.1
 
 
-def central_gradient(f, x: ndarray, h: float = 1e-5) -> ndarray:
+def central_gradient(f, weights: ndarray, h: float = 1e-5) -> ndarray:
     """ Numerical Forward Differentiation for multidimensional arrays
     - Compute the numerical gradient of function f at point x using central difference method
     - Slope of the function at point x approximated using finite differences
     :param f: function to compute the gradient for
-    :param x: point (array) at which to compute the gradient
+    :param weights: weights of the model (array) or array of points at which to compute the gradient
     :param h: small step size for finite difference
     :return: numerical gradient array at point x
     """
     # Initialize gradient array with the same shape as x. The initial values are zeros, which is not important.
-    grad = zeros_like(x)
-    for i in range(x.size):
+    grad = zeros_like(weights)
+    for i in range(weights.size):
         # copy x to avoid modifying the original array
-        x_h1 = x.copy()
-        x_h2 = x.copy()
+        x_h1 = weights.copy()
+        x_h2 = weights.copy()
 
         # central difference
         x_h1[i] += h
@@ -55,20 +55,41 @@ def central_gradient(f, x: ndarray, h: float = 1e-5) -> ndarray:
     return grad
 
 
-def numerical_gradient(f, X: ndarray, h: float = 1e-5) -> ndarray:
+def numerical_gradient(f, WEIGHTS: ndarray, h: float = 1e-5) -> ndarray:
     """ Numerical Forward Differentiation for multidimensional arrays
     - Compute the numerical gradient of function f at point x using central difference method
     - Slope of the function at point x approximated using finite differences
     :param f: function to compute the gradient for
-    :param X: point (array) or array of points at which to compute the gradient
+    :param WEIGHTS: weights of the model (array) or array of points at which to compute the gradient
     :param h: small step size for finite difference
     :return: numerical gradient array at point x
     """
-    if X.ndim == 1:
-        return central_gradient(f, X, h)
+    if WEIGHTS.ndim == 1:
+        return central_gradient(f, WEIGHTS, h)
     else:
         # Initialize gradient array with the same shape as x. The initial values are zeros, which is not important.
-        grad = zeros_like(X)
-        for i in range(X.shape[0]):
-            grad[i] = central_gradient(f, X[i], h)
+        grad = zeros_like(WEIGHTS)
+        for i in range(WEIGHTS.shape[0]):
+            grad[i] = central_gradient(f, WEIGHTS[i], h)
         return grad
+
+
+def gradient_descent(f, init_x: ndarray, lr: float = 0.01, step_num: int = 100) -> tuple[ndarray, ndarray]:
+    """ Gradient Descent Optimization
+    - Perform gradient descent to minimize function f starting from initial point init_x
+    - Update the point iteratively by moving in the direction of the negative gradient scaled by learning rate
+    :param f: function to minimize
+    :param init_x: initial point (array)
+    :param lr: learning rate (step size for each update), which is called alpha in some contexts
+    :param step_num: number of iterations to perform
+    :return: final point after optimization and history of points visited
+    """
+    x = init_x
+    f_values: list = []
+
+    for i in range(step_num):
+        f_values.append(x.copy())
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x, array(f_values)
